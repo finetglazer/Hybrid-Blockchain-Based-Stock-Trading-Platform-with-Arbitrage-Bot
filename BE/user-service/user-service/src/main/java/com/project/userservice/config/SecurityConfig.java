b73ba2beffdb94d0435c4b371867d4d86afb70ec
OLogN <vinhhiepbn2003.work@gmail.com>
2025-02-25 09:30:16 +0700

    feat: "/api/hello" - Tao add API de test ?

---
+++ BE/user-service/user-service/src/main/java/com/project/userservice/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.project.userservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)  // Disable CSRF for testing (not recommended for production)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+               )
+                .build();
+    }
+}
