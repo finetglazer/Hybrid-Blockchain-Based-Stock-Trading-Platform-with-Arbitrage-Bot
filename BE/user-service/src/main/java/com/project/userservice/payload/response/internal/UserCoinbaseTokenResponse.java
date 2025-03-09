db35c6b0f1be335c73d126cdbe2b1f492cd000f6
OLogN <vinhhiepbn2003.work@gmail.com>
2025-03-09 22:12:19 +0700

    fix: add @NoArgsConstructor to files

--- BE/user-service/src/main/java/com/project/userservice/payload/response/internal/UserCoinbaseTokenResponse.java
+++ BE/user-service/src/main/java/com/project/userservice/payload/response/internal/UserCoinbaseTokenResponse.java
@@ -1,10 +1,14 @@
 package com.project.userservice.payload.response.internal;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserCoinbaseTokenResponse {
     private boolean linked;
     private String accessToken;        // If linked + valid
