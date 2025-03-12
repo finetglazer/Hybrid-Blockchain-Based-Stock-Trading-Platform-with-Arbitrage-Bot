a25ff9da724251083bde5797075b878d3c7755f3
nghmyyy <hamynguyen0401@gmail.com>
2025-03-12 11:58:46 +0700

    create sidebar + login successful

--- FE/investwebsite/src/App.jsx
+++ FE/investwebsite/src/App.jsx
@@ -1,18 +1,45 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import ForgetPassword from "./pages/ForgetPassword";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Information from "./pages/Information";
+import Wallet from "./pages/Wallet";
+import NavbarSide from "./pages/NavbarSide/NavbarSide";
 
-const App = () => {
+const Layout = () => {
+  const location = useLocation();
+  const showNavbar = ["/home", "/wallet", "/information"].includes(
+    location.pathname
+  );
+
   return (
-    <Router>
+    <>
+      {showNavbar && <NavbarSide />}
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forget-password" element={<ForgetPassword />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/wallet" element={<Wallet />} />
+        <Route path="/information" element={<Information />} />
       </Routes>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Layout />
     </Router>
   );
 };
+
 export default App;
---
+++ FE/investwebsite/src/pages/Information.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const Information = () => {
+  return <p1>Info</p1>;
+};
+export default Information;
