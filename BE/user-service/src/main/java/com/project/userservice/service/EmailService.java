65d70b53be13b246651c57cd131bdfadbf8c9555
I hate iu <tranhung10122003@gmail.com>
2025-03-27 19:36:30 +0700

    feat: add something

--- BE/user-service/src/main/java/com/project/userservice/service/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.project.userservice.service;
-
-public interface EmailService {
-    void sendVerificationEmail(String toEmail, String token);
-    void sendPasswordResetEmail(String toEmail, String token);
-    void sendConcurrentLoginNotification(String toEmail, String oldIp, String newIp);
-}
--- BE/user-service/src/main/java/com/project/userservice/service/SmsService.java
+++
@@ -1,6 +0,0 @@
-package com.project.userservice.service;
-
-public interface SmsService {
-    String generatePhoneVerificationToken(String phoneNumber);
-    boolean verifyPhoneAuthCredential(String idToken);
-}
