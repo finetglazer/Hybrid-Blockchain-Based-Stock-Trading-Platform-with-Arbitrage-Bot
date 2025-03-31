7d17d0deacdabbace7d8773116b8c77e072f0d8a
OLogN <vinhhiepbn2003.work@gmail.com>
2025-03-31 18:21:54 +0700

    fix: jobrunr

--- BE/account-service/src/main/java/com/accountservice/repository/BalanceHistoryRepository.java
+++ BE/account-service/src/main/java/com/accountservice/repository/BalanceHistoryRepository.java
@@ -4,9 +4,9 @@ import com.accountservice.model.BalanceHistory;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
+import java.util.Date;
 
 @Repository
 public interface BalanceHistoryRepository extends MongoRepository<BalanceHistory, String> {
-    BalanceHistory findBalanceHistoryByAccountIdAndDate(String accountId, LocalDate date);
+    BalanceHistory findBalanceHistoryByAccountIdAndDate(String accountId, Date date);
 }
